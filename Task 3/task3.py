de21a6427855a41599845db686ca02b4946fceee
Shailesh Dhasade <105565788+Mrshailesh4855@users.noreply.github.com>
2025-11-26 17:08:40 +0530

    task3.py

--- Task 3/task3.py
+++ Task 3/task3.py
@@ -4,14 +4,14 @@ import string
 print("Welcome to the Password Generator")
 try:
   length=int(input("Enter the password length: "))
-  if length <= 0:
-        print("Password length must be greater than 0")
+  if length<=0:
+        print("Password length must be greater than 0 !!")
         exit()
 except ValueError:
-    print("Please enter a valid number")
+    print("Please enter a valid number !!!")
     exit()
 
-char_pool =string.ascii_letters+string.digits+string.punctuation
+char=string.ascii_letters+string.digits+string.punctuation
 
-password=''.join(random.choice(char_pool)for _ in range(length))
+password=''.join(random.choice(char)for _ in range(length))
 print(f"Generated Password: {password}")
